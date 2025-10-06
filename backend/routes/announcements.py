0fcbd1a53469f88f25a09d6201c1f275e27b407a
Wladislava1 <146841829+Wladislava1@users.noreply.github.com>
2025-10-06 09:58:51 +0300

    add post

--- backend/routes/announcements.py
+++ backend/routes/announcements.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/announcements", tags=["Announcements"])
+
+class Announcement(BaseModel):
+    type: str
+    title: str
+    city: str
+    description: str
 
 @router.get("/")
 def get_announcements():
@@ -12,3 +19,10 @@ def get_announcements():
 @router.get("/{announcement_id}")
 def get_announcement(announcement_id: int):
     return {"id": announcement_id, "title": "Кошка", "description": "Белая, с серыми пятнами"}
+
+@router.post("/")
+def create_announcement(ad: Announcement):
+    return {
+        "message": "Объявление создано",
+        "data": ad
+    }
